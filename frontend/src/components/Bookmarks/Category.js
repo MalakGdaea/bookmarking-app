fc065db161faad0768e763e6f26c5d1c39a81e65
malak <malak.abogdaya@gmail.com>
2023-04-14 20:51:50 +0300

    hide the bookmarks

--- frontend/src/components/Bookmarks/Category.js
+++ frontend/src/components/Bookmarks/Category.js
@@ -4,25 +4,32 @@ import OptionsList from "./OptionsList";
 import { useState } from "react";
 function Category({ category, setEditedCategoryID, showBookmarkForm, deleteCategory }) {
   const [isOptionListShown, setIsOptionListShown] = useState(false);
+  const [isBookmarksDisplayed, serIsBookmarksDisplayed] = useState(true);
   const toggleOptionsList = () => {
     setIsOptionListShown(!isOptionListShown);
   };
 
+  const toggleBookmarks = () => {
+    serIsBookmarksDisplayed(!isBookmarksDisplayed);
+  }
+
   return (
     <div className="category-container">
       <div className="categoryName">{category.categoryInfo.name}
-        <i className="material-icons icon" onClick={toggleOptionsList}>settings</i>
+        <i className="material-icons settings" onClick={toggleOptionsList}>settings</i>
+        <i className="material-icons arrow" onClick={toggleBookmarks}>{isBookmarksDisplayed ? "keyboard_arrow_down" : "keyboard_arrow_up"} </i>
       </div>
       {isOptionListShown && (
         <OptionsList category={category} setEditedCategoryID={setEditedCategoryID}
           showBookmarkForm={showBookmarkForm} deleteCategory={deleteCategory}
           setIsOptionListShown={setIsOptionListShown} />
       )}
-      <div className="bookmarks">
-        {category.bookmarks.map((bookmark) => {
-          return <Bookmark key={bookmark._id} bookmark={bookmark} />;
-        })}
-      </div>
+      {isBookmarksDisplayed &&
+        <div className="bookmarks">
+          {category.bookmarks.map((bookmark) => {
+            return <Bookmark key={bookmark._id} bookmark={bookmark} />;
+          })}
+        </div>}
     </div>
   );
 }
